bd57a00410efa3396414bac513ffab69fb04cdd0
Evgheni Calcutin <42963334+ecalcutin@users.noreply.github.com>
2019-08-21 20:10:08 +0300

    modified tests

--- src/core/core.module.spec.ts
+++ src/core/core.module.spec.ts
@@ -9,7 +9,7 @@ afterAll(async () => {
     await mongoose.disconnect();
 });
 
-describe('Core module', () => {
+describe('[CORE] :: Instances', () => {
 
     let unitService: UnitService;
     let categoryService: CategoryService;
@@ -17,14 +17,35 @@ describe('Core module', () => {
     let productService: ProductService;
     let stockService: StockService;
 
-    beforeEach(async () => {
+    beforeAll(async () => {
+        await console.log('Initializing')
         const module = await Test.createTestingModule({
             providers: [UnitService, CategoryService, PrototypeService, ProductService, StockService]
         }).compile();
-        stockService = module.get<StockService>(StockService);
+        unitService = module.get(UnitService);
+        categoryService = module.get(CategoryService);
+        prototypeService = module.get(PrototypeService);
+        productService = module.get(ProductService);
+        stockService = module.get(StockService);
     });
 
-    it('Stock service', async () => {
+    it('[Service] :: Unit :: is ready', async () => {
+        expect(unitService).toBeDefined();
+    });
+
+    it('[Service] :: Category :: is ready', async () => {
+        expect(categoryService).toBeDefined();
+    });
+
+    it('[Service] :: Prototype :: is ready', async () => {
+        expect(prototypeService).toBeDefined();
+    });
+
+    it('[Service] :: Product :: is ready', async () => {
+        expect(productService).toBeDefined();
+    });
+
+    it('[Service] :: Stock :: is ready', async () => {
         expect(stockService).toBeDefined();
     });
 
