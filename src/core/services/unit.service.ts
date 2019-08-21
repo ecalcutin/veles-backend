22bd9141b9ee6fbcb44e0d641c9065fe38aabbed
Evgheni Calcutin <42963334+ecalcutin@users.noreply.github.com>
2019-08-21 21:15:43 +0300

    injected model

--- src/core/services/unit.service.ts
+++ src/core/services/unit.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { PaginateModel, PaginateOptions, PaginateResult } from 'mongoose';
+
+import { UnitRef } from '../schemas';
+import { Unit } from '../interfaces';
 
 @Injectable()
-export class UnitService { }
+export class UnitService {
+    constructor(@InjectModel(UnitRef) private readonly unitModel: PaginateModel<Unit>) { }
+}
