9b0bfe4b7af682669e2d59f5c85f89fa7beda950
Evgheni Calcutin <42963334+ecalcutin@users.noreply.github.com>
2019-08-21 23:20:45 +0300

    set global prefix

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   console.log('NODE_ENV: ', process.env.NODE_ENV)
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
   await app.listen(3000);
 }
 bootstrap();
