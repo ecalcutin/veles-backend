a7fade3075ca9f9a429c501489e6d673dc1aa7ed
Evgheni Calcutin <42963334+ecalcutin@users.noreply.github.com>
2019-08-21 22:54:52 +0300

    connecting mongoose module from env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { ConfigModule } from './config/config.module';
+import { ConfigService } from './config/config.service';
+
 import { CoreModule } from './core';
 
 @Module({
-  imports: [CoreModule],
+  imports: [
+    ConfigModule,
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get('DATABASE_URL')
+      }),
+      inject: [ConfigService]
+    }),
+    CoreModule,
+  ],
   controllers: [],
   providers: [],
 })
