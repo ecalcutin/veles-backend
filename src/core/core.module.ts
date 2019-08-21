eae2f16821af526d3c0b182d2ac9463a979022aa
Evgheni Calcutin <42963334+ecalcutin@users.noreply.github.com>
2019-08-21 21:15:36 +0300

    added mongoose module

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { UnitService, CategoryService, PrototypeService, ProductService, StockService } from './services';
+import { UnitRef, UnitSchema } from './schemas';
 
 import { SettingsController } from './settings.controller';
 
 @Module({
+    imports: [
+        MongooseModule.forFeature([
+            { name: UnitRef, schema: UnitSchema }
+        ])
+    ],
     controllers: [SettingsController],
     providers: [UnitService, CategoryService, PrototypeService, ProductService, StockService],
 })
