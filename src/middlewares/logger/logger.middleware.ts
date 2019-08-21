627c885f137083ea85f0819bdaa0f8977c7ca817
Evgheni Calcutin <42963334+ecalcutin@users.noreply.github.com>
2019-08-21 23:53:09 +0300

    use decodeURI

--- src/middlewares/logger/logger.middleware.ts
+++ src/middlewares/logger/logger.middleware.ts
@@ -5,7 +5,7 @@ import { Request, Response } from 'express';
 export class LoggerMiddleware implements NestMiddleware {
     use(req: Request, res: Response, next: Function) {
         console.log('Request...');
-        console.log(`[${req.method}] ${req.url}`)
+        console.log(`[${req.method}] ${decodeURI(req.url)}`)
         next();
     }
 }
