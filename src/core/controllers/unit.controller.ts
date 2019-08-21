5d08377030400e43ed13fccde1c1a4b16c456ef2
Evgheni Calcutin <42963334+ecalcutin@users.noreply.github.com>
2019-08-22 01:38:59 +0300

    added unit controller

--- src/core/controllers/unit.controller.ts
+++ src/core/controllers/unit.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get, Post, Put, Delete, Param, Query } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param, Body, Query } from '@nestjs/common';
 
 import { UnitService } from '../services';
+import { CreateUnitDto, UpdateUnitDto } from '../dto';
+import { Unit } from '../interfaces';
 
 @Controller('settings/units')
 export class UnitController {
@@ -10,5 +12,25 @@ export class UnitController {
     @Get('/')
     async list() {
         return await this.unitService.getAll();
+    }
+
+    @Post('/')
+    async create(@Body() unit: CreateUnitDto): Promise<Unit> {
+        return await this.unitService.create(unit);
+    }
+
+    @Get('/:id')
+    async getById(@Param('id') id: string): Promise<Unit> {
+        return await this.unitService.getById(id);
+    }
+
+    @Put('/:id')
+    async updateById(@Body() unit: UpdateUnitDto, @Param('id') id: string): Promise<Unit> {
+        return await this.unitService.updateById(id, unit);
+    }
+
+    @Delete('/:id')
+    async removeById(@Param('id') id: string): Promise<Unit> {
+        return await this.unitService.removeById(id);
     }
 }
