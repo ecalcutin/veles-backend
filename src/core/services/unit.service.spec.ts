add0ce090b55b35375f0ce332be8420130c36372
Evgheni Calcutin <42963334+ecalcutin@users.noreply.github.com>
2019-08-22 00:32:22 +0300

    updated unit tests

--- src/core/services/unit.service.spec.ts
+++ src/core/services/unit.service.spec.ts
@@ -34,20 +34,16 @@ describe('[UnitService]', () => {
         unitService = module.get(UnitService);
     });
 
-    describe('[UnitService] :: Service', () => {
-        beforeAll(async () => {
-            await unitService.dropCollection();
-        });
-        it('[UnitService] :: Unit collection should be empty', async () => {
-            let units = await unitService.getAll();
-            expect(units.length).toBe(0);
-        });
-        it('[UnitService] :: Should create unit', async () => {
-            let result = await unitService.create({ title: 'метр' });
-            expect(result).toBeDefined();
-            expect(result._id).toBeDefined();
-            expect(result.title).toBe('метр');
-        });
+    it('[UnitService] :: Should drop collection', async () => {
+        await unitService.dropCollection();
+        let units = await unitService.getAll();
+        expect(units.length).toBe(0);
+    });
+    it('[UnitService] :: Should create unit', async () => {
+        let result = await unitService.create({ title: 'метр' });
+        expect(result).toBeDefined();
+        expect(result._id).toBeDefined();
+        expect(result.title).toBe('метр');
     });
 });
 
