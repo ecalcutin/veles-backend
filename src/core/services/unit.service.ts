1c100c991b0bfcb451eedc48f6077132ba90edef
Evgheni Calcutin <42963334+ecalcutin@users.noreply.github.com>
2019-08-21 21:49:07 +0300

    added dumb service

--- src/core/services/unit.service.ts
+++ src/core/services/unit.service.ts
@@ -1,11 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Body } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { PaginateModel, PaginateOptions, PaginateResult } from 'mongoose';
 
 import { UnitRef } from '../schemas';
 import { Unit } from '../interfaces';
+import { CreateUnitDto } from '../dto';
 
 @Injectable()
 export class UnitService {
     constructor(@InjectModel(UnitRef) private readonly unitModel: PaginateModel<Unit>) { }
+
+    async create(@Body() unit: CreateUnitDto): Promise<Unit> {
+        return await new this.unitModel(unit).save();
+    }
+
+    async list(): Promise<Unit[]> {
+        return await this.unitModel.find().exec();
+    }
+
 }
