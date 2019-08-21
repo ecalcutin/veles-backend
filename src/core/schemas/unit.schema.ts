c309b75151e4aee182c5c29758a1544dc07a4065
Evgheni Calcutin <42963334+ecalcutin@users.noreply.github.com>
2019-08-21 20:12:12 +0300

    added unit schema

---
+++ src/core/schemas/unit.schema.ts
@@ -0,0 +1,18 @@
+import { Schema } from 'mongoose';
+import paginate from 'mongoose-paginate-v2';
+
+export const UnitRef = "Unit";
+export const UnitSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        unique: true
+    }
+},
+    {
+        timestamps: true
+
+    }
+);
+
+UnitSchema.plugin(paginate);
