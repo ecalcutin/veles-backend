2856b96d285d49d1cb29380ba5f6cec795d6ba53
Evgheni Calcutin <42963334+ecalcutin@users.noreply.github.com>
2019-08-21 19:43:51 +0300

    group providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { CoreModule } from './core';
-import { UnitProvider } from './core/unit.provider';
-import { StockProvider } from './core/stock.provider';
 
 @Module({
   imports: [CoreModule],
   controllers: [],
-  providers: [StockProvider],
+  providers: [],
 })
 export class AppModule { }
--- src/core/index.ts
+++ src/core/index.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
-import { UnitProvider } from './unit.provider';
-import { CategoryProvider } from './category.provider';
-import { PrototypeProvider } from './prototype.provider';
-import { StockProvider } from './stock.provider';
+import { UnitProvider } from './providers/unit.provider';
+import { CategoryProvider } from './providers/category.provider';
+import { PrototypeProvider } from './providers/prototype.provider';
+import { StockProvider } from './providers/stock.provider';
 
 @Module({
     providers: [UnitProvider, CategoryProvider, PrototypeProvider, StockProvider],
