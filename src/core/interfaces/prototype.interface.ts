67942aa9382522a0df23dd2ca9792910d321cda1
Evgheni Calcutin <42963334+ecalcutin@users.noreply.github.com>
2019-08-22 01:09:36 +0300

    added prototype interface

--- src/core/interfaces/prototype.interface.ts
+++ src/core/interfaces/prototype.interface.ts
@@ -1,5 +1,11 @@
 import { Document } from 'mongoose';
 
 export interface Prototype extends Document {
+    readonly title: string;
+    readonly _category: string;
+    readonly _unit: string;
 
+    readonly price_retail: number;
+    readonly price_wholesale: number;
 }
+
