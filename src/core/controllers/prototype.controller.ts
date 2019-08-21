5d62ef3692a8327279f3b59e752d480bd448d7e0
Evgheni Calcutin <42963334+ecalcutin@users.noreply.github.com>
2019-08-22 01:39:59 +0300

    added prototype controller

--- src/core/controllers/prototype.controller.ts
+++ src/core/controllers/prototype.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get, Post, Put, Delete, Param, Query } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param, Body, Query } from '@nestjs/common';
 
 import { PrototypeService } from '../services';
+import { Prototype } from '../interfaces';
+import { CreatePrototypeDto, UpdatePrototypeDto } from '../dto';
 
 @Controller('settings/prototypes')
 export class PrototypeController {
@@ -10,5 +12,25 @@ export class PrototypeController {
     @Get('/')
     async list() {
         return await this.prototypeService.getAll();
+    }
+
+    @Post('/')
+    async create(@Body() prototype: CreatePrototypeDto): Promise<Prototype> {
+        return await this.prototypeService.create(prototype);
+    }
+
+    @Get('/:id')
+    async getById(@Param('id') id: string): Promise<Prototype> {
+        return await this.prototypeService.getById(id);
+    }
+
+    @Put('/:id')
+    async updateById(@Body() prototype: UpdatePrototypeDto, @Param('id') id: string): Promise<Prototype> {
+        return await this.prototypeService.updateById(id, prototype);
+    }
+
+    @Delete('/:id')
+    async removeById(@Param('id') id: string): Promise<Prototype> {
+        return await this.prototypeService.removeById(id);
     }
 }
