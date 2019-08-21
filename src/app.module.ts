37d473a7161362684382e8105d06ecec0a3d5b4a
Evgheni Calcutin <42963334+ecalcutin@users.noreply.github.com>
2019-08-21 23:01:25 +0300

    config module to index

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
-import { ConfigModule } from './config/config.module';
-import { ConfigService } from './config/config.service';
+import { ConfigModule, ConfigService } from './config';
+
 
 import { CoreModule } from './core';
 
