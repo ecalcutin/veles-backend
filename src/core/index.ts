525917d360ebb64a2021cb2908e98d47f9bcbca1
Evgheni Calcutin <42963334+ecalcutin@users.noreply.github.com>
2019-08-21 19:44:34 +0300

    renamed some shit

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,11 +1,1 @@
-import { Module } from '@nestjs/common';
-import { UnitProvider } from './providers/unit.provider';
-import { CategoryProvider } from './providers/category.provider';
-import { PrototypeProvider } from './providers/prototype.provider';
-import { StockProvider } from './providers/stock.provider';
-
-@Module({
-    providers: [UnitProvider, CategoryProvider, PrototypeProvider, StockProvider],
-    exports: [StockProvider]
-})
-export class CoreModule { }
+export { CoreModule } from './core.module';
