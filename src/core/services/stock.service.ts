c7dea3de3be00e6e9f08a926a1ca6156bfca54a4
calcutinE <42960376+calcutinE@users.noreply.github.com>
2019-09-06 11:51:55 +0300

    added stock services

--- src/core/services/stock.service.ts
+++ src/core/services/stock.service.ts
@@ -4,6 +4,19 @@ import { Model } from 'mongoose';
 
 import { StockRef } from '../schemas';
 import { Stock } from '../interfaces';
+import { CreateStockDto, UpdateStockDto } from '../dto';
 
 @Injectable()
-export class StockService {}
+export class StockService {
+  constructor(
+    @InjectModel(StockRef) private readonly stockModel: Model<Stock>,
+  ) {}
+
+  async create(stock: CreateStockDto): Promise<Stock> {
+    return await new this.stockModel(stock).save();
+  }
+
+  async getAll(): Promise<Stock[]> {
+    return await this.stockModel.find().exec();
+  }
+}
