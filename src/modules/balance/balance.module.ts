86d55c6a41495bf48506e3821dce46fd3961ab1a
Evgheni Calcutin <42963334+ecalcutin@users.noreply.github.com>
2019-09-07 21:43:32 +0300

    added db connection

--- src/modules/balance/balance.module.ts
+++ src/modules/balance/balance.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { BalanceService } from './balance.service';
 import { BalanceController } from './balance.controller';
 
+import { ProductRef, ProductSchema } from '../settings/schemas';
+
 @Module({
-    imports: [],
+    imports: [
+        MongooseModule.forFeature([
+            { name: ProductRef, schema: ProductSchema }
+        ]),
+    ],
     controllers: [BalanceController],
     providers: [BalanceService]
 })
