a03bc672f716e52d5f4ee2d2f1e5a8a32cce70cd
Evgheni Calcutin <42963334+ecalcutin@users.noreply.github.com>
2019-09-07 20:28:25 +0300

    removed redundand code

--- src/modules/settings/services/index.ts
+++
@@ -1,4 +0,0 @@
-export { CategoryService } from './category.service';
-export { PrototypeService } from './prototype.service';
-export { StockService } from './stock.service';
-export { ProductService } from './product.service';
