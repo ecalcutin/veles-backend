21e8d71674e485164a4bd2fd3d302716df9e35f1
calcutinE <42960376+calcutinE@users.noreply.github.com>
2019-09-11 18:05:56 +0300

    added balance schema

--- src/modules/settings/settings.module.ts
+++ src/modules/settings/settings.module.ts
@@ -12,20 +12,18 @@ import {
 
 import { SettingsController } from './settings.controller';
 import { SettingsService } from './settings.service';
+import { BalanceRef, BalanceSchema } from '../balance/schemas/balance.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: CategoryRef, schema: CategorySchema },
       { name: StockRef, schema: StockSchema },
-      { name: ProductRef, schema: ProductSchema }
+      { name: ProductRef, schema: ProductSchema },
+      { name: BalanceRef, schema: BalanceSchema },
     ]),
   ],
-  controllers: [
-    SettingsController,
-  ],
-  providers: [
-    SettingsService
-  ],
+  controllers: [SettingsController],
+  providers: [SettingsService],
 })
-export class SettingsModule { }
+export class SettingsModule {}
