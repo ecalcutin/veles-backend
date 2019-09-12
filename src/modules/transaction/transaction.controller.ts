300142bd6602375c958048c952b95877975d9582
Evgheni Calcutin <42963334+ecalcutin@users.noreply.github.com>
2019-09-12 22:27:09 +0300

    added query parser to controller

--- src/modules/transaction/transaction.controller.ts
+++ src/modules/transaction/transaction.controller.ts
@@ -3,8 +3,14 @@ import { TransactionService } from './transaction.service';
 
 @Controller('transactions')
 export class TransactionController {
-  constructor(private readonly transactionService: TransactionService) {}
+  constructor(private readonly transactionService: TransactionService) { }
 
   @Get('/')
-  async fetchBalance(@Query() options) {}
+  async fetchBalance(
+    @Query('stock_id') stock_id: string,
+    @Query('startDate') startDate: string,
+    @Query('endDate') endDate: string
+  ) {
+    return await this.transactionService.calculateBalances(stock_id, startDate, endDate);
+  }
 }
