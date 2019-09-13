7b34bada284a54a34ce394c413073d3fdd96214c
calcutinE <42960376+calcutinE@users.noreply.github.com>
2019-09-13 17:39:36 +0300

    adjusted interface & updated todo

--- src/modules/transaction/interfaces/transaction.interface.ts
+++ src/modules/transaction/interfaces/transaction.interface.ts
@@ -3,7 +3,6 @@ import { Document } from 'mongoose';
 export interface Transaction extends Document {
   readonly _product: string;
   readonly _stock: string;
-  readonly income: number;
-  readonly outcome: number;
+  readonly change: number;
   readonly action: string;
 }
