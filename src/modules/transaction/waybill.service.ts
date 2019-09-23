f2b77340532342f5bdd079da96dd5980ba0c9bbc
calcutinE <42960376+calcutinE@users.noreply.github.com>
2019-09-23 15:27:04 +0300

    creating waybill service

--- src/modules/transaction/waybill.service.ts
+++ src/modules/transaction/waybill.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import moment from 'moment';
 
 import { WaybillRef } from './schemas';
 import { Waybill } from './interfaces';
+
+type TWaybill = {
+  _source: string;
+  _destination: string;
+  date: string;
+  items: Array<any>;
+};
 
 @Injectable()
 export class TransactionService {
@@ -12,5 +18,7 @@ export class TransactionService {
     @InjectModel(WaybillRef) private readonly waybillModel: Model<Waybill>,
   ) {}
 
-  
+  async createWaybill(waybill: TWaybill): Promise<Waybill> {
+    return await new this.waybillModel(waybill).save();
+  }
 }
