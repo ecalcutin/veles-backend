6b4e81b35a6c3032ed54c365b5bb211ce13b6450
calcutinE <42960376+calcutinE@users.noreply.github.com>
2019-09-23 16:09:47 +0300

    fixed waybill schema

--- src/modules/transaction/interfaces/waybill.interface.ts
+++ src/modules/transaction/interfaces/waybill.interface.ts
@@ -1,3 +1,13 @@
 import { Document } from 'mongoose';
 
-export interface Waybill extends Document {}
+interface Product {
+  readonly _id: string;
+  readonly quantity: number;
+}
+export interface Waybill extends Document {
+  readonly actionTitle: string;
+  readonly _source: string;
+  readonly _destination: string;
+  readonly date: string;
+  readonly products: Product[];
+}
--- src/modules/transaction/schemas/waybill.schema.ts
+++ src/modules/transaction/schemas/waybill.schema.ts
@@ -6,23 +6,22 @@ import { ProductRef, StockRef } from '../../settings/schemas';
 export const WaybillRef = 'Waybill';
 export const WaybillSchema = new Schema(
   {
+    actionTitle: String,
     _source: {
       type: Schema.Types.ObjectId,
       ref: StockRef,
-      required: true,
     },
     _destination: {
       type: Schema.Types.ObjectId,
       ref: StockRef,
-      required: true,
     },
     date: {
       type: String,
       default: () => moment().format('YYYY-MM-DD'),
     },
-    items: [
+    products: [
       {
-        product: {
+        _id: {
           type: Schema.Types.ObjectId,
           ref: ProductRef,
           required: true,
