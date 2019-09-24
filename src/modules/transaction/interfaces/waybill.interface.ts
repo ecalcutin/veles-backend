63073120b6d5fa1b683e69bebf2f94dbc42916c2
Evgheni Calcutin <42963334+ecalcutin@users.noreply.github.com>
2019-09-24 23:27:08 +0300

    interface drafted

--- src/modules/transaction/interfaces/waybill.interface.ts
+++ src/modules/transaction/interfaces/waybill.interface.ts
@@ -1,13 +1,13 @@
 import { Document } from 'mongoose';
 
 interface Product {
-  readonly _id: string;
+  readonly _id: any;
   readonly quantity: number;
 }
 export interface Waybill extends Document {
   readonly action: string;
-  readonly _source: string;
-  readonly _destination: string;
+  readonly _source: any;
+  readonly _destination: any;
   readonly date: string;
   readonly type: string;
   readonly products: Product[];
