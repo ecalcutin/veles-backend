9759f9da2ca600d2b45202b8a1bf112c6de37a57
calcutinE <42960376+calcutinE@users.noreply.github.com>
2019-09-24 14:06:24 +0300

    added service draft

--- src/modules/document/document.service.ts
+++ src/modules/document/document.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import PizZip from 'pizzip';
+import DocxTemplater from 'docxtemplater';
+import { readFileSync, writeFileSync } from 'fs';
+import { resolve } from 'path';
 
 @Injectable()
-export class DocumentService {}
+export class DocumentService {
+  constructor() {}
+
+  async prepareWaybillDocument() {
+    let content = readFileSync(resolve(__dirname, 'input.docx'), 'binary');
+
+    let zip = new PizZip(content);
+    let doc = new DocxTemplater();
+    doc.loadZip(zip);
+
+    doc.setData({});
+
+    try {
+    } catch (err) {}
+
+    let buff = doc.getZip().generate({ type: 'nodebuffer' });
+    writeFileSync(resolve(__dirname, 'output.docx'), buff);
+  }
+}
