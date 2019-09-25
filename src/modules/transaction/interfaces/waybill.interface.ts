376b2af699e40d1f86dd27b55ecc94cabaa8bc83
calcutinE <42960376+calcutinE@users.noreply.github.com>
2019-09-25 14:59:36 +0300

    adjusted waybill interface

--- src/modules/transaction/interfaces/waybill.interface.ts
+++ src/modules/transaction/interfaces/waybill.interface.ts
@@ -2,7 +2,13 @@ import { Document } from 'mongoose';
 
 interface Product {
   readonly original: {
-    _id: string;
+    readonly _id: string;
+    readonly title: string;
+    readonly category: string;
+    readonly unit: string;
+    readonly price_wholesale: number;
+    readonly price_retail: number;
+    readonly quantity: number;
   };
   readonly title: string;
   readonly category: string;
@@ -12,9 +18,10 @@ interface Product {
 }
 export interface Waybill extends Document {
   readonly action: string;
+  readonly type: string;
   readonly _source: any;
   readonly _destination: any;
   readonly date: string;
-  readonly type: string;
+
   readonly products: Product[];
 }
