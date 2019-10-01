8413f362f42c77be159697289d5e404fb9c9ef77
calcutinE <42960376+calcutinE@users.noreply.github.com>
2019-10-01 16:24:01 +0300

    removed shit

--- src/modules/settings/interfaces/category.interface.ts
+++
@@ -1,6 +0,0 @@
-import { Document } from 'mongoose';
-
-export interface Category extends Document {
-  readonly title: string;
-  readonly unit: string;
-}
--- src/modules/settings/schemas/index.ts
+++ src/modules/settings/schemas/index.ts
@@ -1,3 +1,2 @@
-export { CategoryRef, CategorySchema } from './category.schema';
 export { StockRef, StockSchema } from './stock.schema';
 export { ProductRef, ProductSchema } from './product.schema';
